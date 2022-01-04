a5bdee590e836d643e2accbf08ba57263ea225be
Onur Gültekin <onurgultekin@Onurs-MacBook-Pro.local>
2022-01-04 22:30:45 +0300

    add dotenv package, get weather info from openweatherapi

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
-const http = require('http');
+const request = require('request');
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const cities = [
   { name: 'Istanbul', country: 'Turkey' },
   { name: 'New York', country: 'USA' },
-  { name: 'London', country: 'England' }
-];
+  { name: 'London', country: 'England' },
+  { name: 'sadas', country: 'England' }
+]; // Cities could be collected from a mongoDB collection.
 const app = express();
+
+// Dotend config
+require('dotenv').config()
+
+//Get API key from env file
+const apiKey = `${process.env.API_KEY}`;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -16,27 +23,15 @@ app.get('/api/cities', function (request, response) {
   response.send(cities);
 });
 
-app.get('/api/weather/:name', function (request, response) {
-  const cityName = request.params.name;
-  let found = false;
-  cities.forEach(city => {
-    if (city.name == cityName) {
-      found = true;
-      const resp = {
-        city: city,
-        weather: randomIntFromInterval(-30, 50)
-      }
-      response.json(resp);
-    }
-  });
-  if (!found) {
-    response.json({ error: "City not found" });
-  }
+app.get('/api/weather/:name', function (req, res) {
+  const cityName = req.params.name;
+  let url = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${apiKey}`;
+  request(url, function (err, response, body) {
+    console.log(err, response, body);
+    let weather = JSON.parse(body);
+    res.json(weather);
+  })
 });
-
-function randomIntFromInterval(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min)
-}
 
 app.listen(5000, function () {
   console.log("App is running on port 5000");
